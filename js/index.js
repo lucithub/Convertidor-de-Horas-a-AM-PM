a84ea6b184a2bef8c7f4d504a6ab24eabfeb1d91
lucithub <lucasvelez2013@gmail.com>
2024-12-14 01:24:00 -0300

    fix hour problems

--- js/index.js
+++ js/index.js
@@ -9,20 +9,31 @@ function convertirHora() {
     else{
         horaConvertida = hora * 1;
     }
+    //
     if(hora === '00'){
         resultado = 'Son las 12:' + minutos + ' AM';
         document.body.style.backgroundColor = "rgb(64, 85, 89)";
         document.getElementById('imageDayNight').src = "/images/luna.png";
     }
-    else if(hora >= 12){
-        resultado = 'Son las: ' + horaConvertida + ':' + minutos + ' PM';
+    else if(hora >= 0 && hora <= 5){
+        resultado = 'Son las: ' + horaConvertida + ':' + minutos + ' AM';
         document.body.style.backgroundColor = "rgb(64, 85, 89)";
         document.getElementById('imageDayNight').src = "/images/luna.png";
     }
-    else{
+    else if(hora >= 6 && hora <= 11){
         resultado = 'Son las: ' + horaConvertida + ':' + minutos + ' AM';
         document.body.style.backgroundColor = "rgb(224, 175, 97)";
         document.getElementById('imageDayNight').src = "/images/dia.png";
+    }
+    else if(hora >= 12 && hora <= 17){
+        resultado = 'Son las: ' + horaConvertida + ':' + minutos + ' PM';
+        document.body.style.backgroundColor = "rgb(224, 175, 97)";
+        document.getElementById('imageDayNight').src = "/images/dia.png";
+    }
+    else if(hora >= 18 && hora <= 23){
+        resultado = 'Son las: ' + horaConvertida + ':' + minutos + ' PM';
+        document.body.style.backgroundColor = "rgb(64, 85, 89)";
+        document.getElementById('imageDayNight').src = "/images/luna.png";
     }
     document.getElementById('resultado').innerHTML = resultado;
 }
