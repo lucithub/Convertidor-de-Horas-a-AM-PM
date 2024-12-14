26176933343aae50613baef5052409000bc47097
lucithub <lucasvelez2013@gmail.com>
2024-12-14 01:17:59 -0300

    reorganization

--- js/index.js
+++ js/index.js
@@ -12,17 +12,17 @@ function convertirHora() {
     if(hora === '00'){
         resultado = 'Son las 12:' + minutos + ' AM';
         document.body.style.backgroundColor = "rgb(64, 85, 89)";
-        document.getElementById('imageDayNight').src = "images/luna.png";
+        document.getElementById('imageDayNight').src = "/images/luna.png";
     }
     else if(hora >= 12){
         resultado = 'Son las: ' + horaConvertida + ':' + minutos + ' PM';
         document.body.style.backgroundColor = "rgb(64, 85, 89)";
-        document.getElementById('imageDayNight').src = "images/luna.png";
+        document.getElementById('imageDayNight').src = "/images/luna.png";
     }
     else{
         resultado = 'Son las: ' + horaConvertida + ':' + minutos + ' AM';
         document.body.style.backgroundColor = "rgb(224, 175, 97)";
-        document.getElementById('imageDayNight').src = "images/dia.png";
+        document.getElementById('imageDayNight').src = "/images/dia.png";
     }
     document.getElementById('resultado').innerHTML = resultado;
 }
